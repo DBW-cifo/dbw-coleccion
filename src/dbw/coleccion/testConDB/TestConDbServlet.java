0b3f50de74fccee577ba975ee9a2b86dab6bacf4
DBW-cifo <55205836+DBW-cifo@users.noreply.github.com>
2020-01-15 13:09:37 +0100

    Add files via upload

--- src/dbw/coleccion/testConDB/TestConDbServlet.java
+++ src/dbw/coleccion/testConDB/TestConDbServlet.java
@@ -1,4 +1,4 @@
-package dbw.coleccion.testConDB;
+package dbw.filatelias.testConDB;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -26,7 +26,7 @@ import javax.servlet.http.HttpServletResponse;
     		protected void doGet(HttpServletRequest request, HttpServletResponse response)
     		       throws ServletException, IOException {
     		String cadConexion = "jdbc:mysql://localhost:3306/"; 
-    		String bd = "coleccion";
+    		String bd = "filatelias";
     		String usuario = "root";
     		String pass = "kGd25870";
     		
